c645bc252b6ce8eefd0a25811655a60a6a065b27
Meep10 <48997893+Meep10@users.noreply.github.com>
2020-04-17 19:20:50 +0300

    Entity Edit

--- .github/workflows/entity.js
+++ .github/workflows/entity.js
@@ -1,8 +1,13 @@
 class Entity {
-    constructor(name, description, entityType) {
+    constructor(name, description, image, x, y, width, height, entityType) {
         this.id = Math.floor((Math.random() * 1000000));
         this.name = name;
         this.description = description;
+        this.image = image;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
         this.entityType = entityType;
     }
 }
